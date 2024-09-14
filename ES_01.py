27a96ddcc8e1764e6cbbc8863df47d9038ceae6d
FraGit69 <francesco.re@itiscuneo.eu>
2024-09-14 13:42:09 +0200

    raddoppio i valori della lista

---
+++ ES_01.py
@@ -0,0 +1,7 @@
+# Da lista1 = [1, 2, 3, 4, 5], creo lista2 che ha tutti i valori raddoppiati
+lista1 = [1, 2, 3, 4, 5]
+
+#per tutti i valori della lista1 li raddoppio e li salvo nella lista2
+lista2 = [n*2 for n in lista1]
+
+print(lista2)
